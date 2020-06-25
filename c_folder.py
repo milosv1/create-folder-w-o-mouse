a2380d32fca8b5b7011f2da8352089d55b8689ce
Milos <milosvuksanovic53@gmail.com>
2020-06-25 22:12:17 +1000

    Checking if user platform is windows. needs work

--- c_folder.py
+++ c_folder.py
@@ -1,1 +1,12 @@
 #Objective of this is to create a simple script to create folders without use of mouse.
+import sys
+import platform
+import os
+def c_folder_windows():
+    if sys.platform.startswith("win32"):
+        print("on windows")
+    else:
+        print("not on windows") 
+
+
+c_folder_windows()           
