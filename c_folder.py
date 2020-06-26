0bdbaa5031c15aece15ed4e30301800874abb8bb
Milos <milosvuksanovic53@gmail.com>
2020-06-26 23:08:44 +1000

    Key combo working

--- c_folder.py
+++ c_folder.py
@@ -2,23 +2,42 @@
 import sys
 import platform
 import os
+from pynput import keyboard
+
 
 get_platform = sys.platform
 
-def c_folder_windows():
+#checks the key combo -> tab + n
+KEY_COMBO = {keyboard.Key.tab, keyboard.KeyCode.from_char('n')}
+current = set()
+
+def c_folder_windows(key):
     if sys.platform.startswith("win32"):
-        print("on windows")
-    else:
-        print("not on windows") 
+        if key in KEY_COMBO:
+            current.add(key)
+            if all(k in current for k in KEY_COMBO):
+                print(f"{KEY_COMBO} working!")
+    if key == keyboard.Key.esc:
+        print("Stopped")
+        listener.stop()
 
-
+         
 def c_folder_mac():
     if sys.platform.startswith("darwin"):
         print("platform you're on is macOS")
-        
     else:
          print("not macOS ")    
          print(f"current plaform: {get_platform}")
+
+
+def on_release(key):
+    try:
+        current.remove(key)
+    except KeyError:
+        pass
+
+with keyboard.Listener(on_press= c_folder_windows, on_release=on_release) as listener:
+    listener.join()
 
 
 c_folder_windows()   
